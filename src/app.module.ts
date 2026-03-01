bc286c0f77b9e9980b85d18f2d5598a108fab51c
tklasnakov <t.klasnakov@gmail.com>
2026-03-01 11:04:48 +0200

    create guests module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GuestsModule } from './guests/guests.module';
 
 @Module({
-  imports: [],
+  imports: [GuestsModule],
   controllers: [AppController],
   providers: [AppService],
 })
