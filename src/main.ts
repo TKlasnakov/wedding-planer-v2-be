e1205e24b02fdaec6dd5475448e3eea608d4259a
tklasnakov <t.klasnakov@gmail.com>
2026-03-11 19:08:05 +0200

    add console.logs for debuging

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log(process.env.CORS_ORIGIN);
+  console.log(process.env);
   app.enableCors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
